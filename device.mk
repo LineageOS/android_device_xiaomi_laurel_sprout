15b3ee2af8b6d4b4ada7477b7d7469787a451e21
Skyblueborb <tomaszborbely0710@gmail.com>
2025-07-28 18:46:44 +0200

    laurel_sprout: Switch to two-stage init mounting
    * Stop handling vendor mounting from kernel, first stage init is gonna
      do that, together with system (which is now /system)
    * Use correct mount flags coming from kernel dts
    * Move system, vendorand metadata mount points to /dev/block/by-name/, we don't
      have bootdevice symlink to that point in init
    * Copy fstab to recovery for first stage init
    
    Change-Id: Iaa0ecc3f13c3cfd3fe357ca509549d0f68a29cf0

--- device.mk
+++ device.mk
@@ -65,7 +65,8 @@ TARGET_BOARD_FASTBOOT_INFO_FILE := $(DEVICE_PATH)/fastboot-info.txt
 
 # Rootdir
 PRODUCT_PACKAGES += \
-    fstab.qcom
+    fstab.qcom \
+    fstab.qcom.ramdisk
 
 PRODUCT_PACKAGES += \
     init.device.rc \
